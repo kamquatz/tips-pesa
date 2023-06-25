5056f1c8e3e2ee3da690574c6c3052b0df30d3a3
Dennis Muga <dennis.mutethia1@accesscfa.com>
2023-06-25 13:33:24 +0300

    nav full width

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,17 @@ import { footerLinks } from "@/constants"
 const Footer = () => {
     return (
         <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
-            <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+            <div className="flex justify-between items-center flex-wrap gap-5 sm:px-16 px-6 py-10">
                 <div className="footer__links">
+                    <Link href='/' className='footer__link justify-left'>
+                        <Image
+                            src='/tipspesa.png'
+                            alt='TipsPesa Logo'
+                            width={32}
+                            height={32}
+                            className='object-contain'
+                        />Tips Pesa &copy;{new Date().getFullYear()}
+                    </Link>
                     {footerLinks.map((link) => (
                         <div key={link.title} className="footer__link">
                             <h3 className="font-bold">{link.title}</h3>
--- components/MatchCard.tsx
+++ components/MatchCard.tsx
@@ -14,12 +14,12 @@ const MatchCard = ({ match }: MarchCardProps) => {
             <div className='relative flex w-full'>
                 <div className='flex w-full justify-between text-gray'>
                     <div className='flex flex-col justify-center items-center gap-2'>
-                        <p className='text-[14px]'>
+                        <p className='text-[10px]'>
                             {kickoff_date}
                         </p>
                     </div>
                     <div className='flex flex-col justify-center items-center gap-2'>
-                        <p className='text-[14px]'>
+                        <p className='text-[10px]'>
                             {kickoff_time}
                         </p>
                     </div>
@@ -30,27 +30,27 @@ const MatchCard = ({ match }: MarchCardProps) => {
                 {home} vs {away}
             </small>
 
-            <p className='flex w-full justify-center font-extrabold'>
+            <p className='flex w-full justify-center font-extrabold text-blue-600'>
                 {prediction}
             </p>
 
             <div className='relative flex w-full mt-2'>
                 <div className='flex w-full justify-between text-gray'>
                     <div className='flex flex-col justify-center items-center gap-2'>
-                        <p className='text-[14px] font-semibold'>
+                        <p className='text-[10px] font-semibold'>
                             {probability ? probability + '%' : ''}
                         </p>
                     </div>
 
                     <div className='flex flex-col justify-center items-center gap-2'>
-                        <p className={`text-[14px] font-bold ${status === 'WON' ? 'text-green-500' : status === 'LOST' ? 'text-red-500' : ''}`}>
+                        <p className={`text-[12px] font-bold ${status === 'WON' ? 'text-green-500' : status === 'LOST' ? 'text-red-500' : ''}`}>
                             {result ? result + ' ' + status : ''}
                         </p>
                     </div>
 
 
                     <div className='flex flex-col justify-center items-center gap-2'>
-                        <p className='text-[14px] font-semibold'>
+                        <p className='text-[10px] font-semibold'>
                             {odd ? 'x' + odd : ''}
                         </p>
                     </div>
