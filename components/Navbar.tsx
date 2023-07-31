0ee408c18df3ca258489d407e717e8fd19af644c
Dennis Muga <dennis.mutethia1@accesscfa.com>
2023-07-31 13:24:47 +0300

    hide subscription btn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,12 +22,12 @@ const Navbar = () => {
           />
         </Link>
 
-        <CustomButton
+        {/* <CustomButton
           title="Subscribe to Premium"
           containerStyles="bg-yellow-500 text-white rounded-full flex justify-center items-center"
           handleClick={() => setIsOpen(true)}
           rightIcon='/premium-membership.png'
-        />
+        /> */}
 
         <Subscription isOpen={isOpen} closeModal={() => setIsOpen(false)} />
 
